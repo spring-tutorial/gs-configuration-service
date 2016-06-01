5fec58636b40a9bef8e428f07691d29e21020c29
damiankam <demon.kaminsky@gmail.com>
2016-06-01 19:06:24 +0300

    add sources

---
+++ README.md
@@ -0,0 +1,1 @@
+* Spring tutorial https://spring.io/guides/gs/centralized-configuration/
---
+++ src/main/java/hello/ConfigServiceApplication.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by dima on 6/1/16.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+}
